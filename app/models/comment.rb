a97b852ea2938523c32ba590094df2e4408db067
wendy <wendycaner@gmail.com>
2015-04-09 22:48:32 +0800

    Add validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ActiveRecord::Base
+  belongs_to :article
+  belongs_to :user
+
+  validates :ty, presence: true, inclusion: { in: ['as', 'is'] }
+  varldates :si, presence: true
 end
