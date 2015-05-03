867624e0cbb8893796ddcf235d3ffd4916296315
wendy <wendycaner@gmail.com>
2015-05-03 17:47:02 +0800

    Add pdf upload

--- Gemfile
+++ Gemfile
@@ -44,4 +44,4 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
-
+gem 'byebug'
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,10 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   
+  validates :title, presence: true
+  validates :pdf, presence: true
+  
   mount_uploader :pdf, PdfUploader
 
-  self.per_page = 1
+  self.per_page = 8
 end
